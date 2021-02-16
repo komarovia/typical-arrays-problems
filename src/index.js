9a1ad0db5347c5e17fb91e48bf082372e6c2c37f
komarovia <komarovia@gmail.com>
2021-02-16 13:37:40 +0300

    feat: completed task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array === 'undefined' || array.length === 0) {return 0}
+    else {array.sort((a, b) => b - a);
+        array.reverse();
+    return array[0];}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array === 'undefined' || array.length === 0) {return 0}
+    else {array.sort((a, b) => b - a);
+    return array[0];}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(typeof array === 'undefined' || array.length === 0) {return 0}
+    else {let value = array.reduce((sum, current) => sum + current, 0);
+    value = value / array.length;
+    return value;}
 }
